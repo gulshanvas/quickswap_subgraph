3334590d2bdf4f1cd8b8de9ec0c052fb907a957c
Gulshan Vasnani <gulshanslvasnani@gmail.com>
2022-11-11 01:06:59 +0530

    Added gitignore

---
+++ src/Pair.ts
@@ -0,0 +1,37 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { Contract, PairCreated } from "../generated/Contract/Contract"
+import { Factory } from "../generated/schema"
+
+
+export function handleSwap(event: Mint): void {
+
+  /**
+   * TODO:
+   * 1. Creates or updates `Pair` and `UserPair` entity
+   */
+
+}
+
+
+export function handleBurn(event: Burn): void {
+
+  /**
+   * TODO:
+   * 1. Creates or updates `Pair` and `UserPair` entity
+   */
+
+}
+
+export function handleSwap(event: Swap): void {
+  /**
+   * TODO:
+   * 1. Creates or updates `Pair` and `UserPair` entity
+   */
+}
+
+export function handleSync(event: Sync): void {
+  /**
+   * TODO:
+   * 1. Creates or updates `Pair` and `UserPair` entity
+   */
+}
